f523d79adfc5169c3b1168c1df1112e47954a327
karthikgpi1 <karthikgopi024@gmail.com>
2022-09-12 23:53:50 +0530

    location and date

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ const api = {
   base: "https//api.openweathermap.org/data/2.5/"
 }
 
+
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August",
+  "September", "October", "November", "Decembar"];
+
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`
+  }
   return (
     <div className="app">
       <main>
@@ -13,6 +28,10 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."/>
+         </div>
+         <div className="location-box">
+           <div className="location">vellore, tamilnadu</div>
+           <div className="date">{dateBuilder(new Date())}</div>
          </div>
       </main>
     </div>
