c17ff7495b2ae9b43dd06c26b2d28ac4397d5ec6
karthikgpi1 <karthikgopi024@gmail.com>
2022-09-12 23:35:03 +0530

    search bar css

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ const api = {
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <main>
+         <div className="search-box">
+            <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."/>
+         </div>
+      </main>
     </div>
   );
 }
